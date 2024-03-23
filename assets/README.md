27a85e6b3309d79846afc3a19eca53ce2d78b678
pedropark99 <pedropark99@gmail.com>
2024-03-23 12:32:20 -0300

    Add example of templates

---
+++ assets/README.md
@@ -0,0 +1,11 @@
+The LightRoom template files present in this folder, were collected
+from `robertdebock` GitHub repository.
+
+<https://github.com/robertdebock/lr-robertdebock>
+
+These files are used as examples of LightRoom template files, and they
+are used specially as inputs for the unit tests of the library.
+
+These files from `robertdebock` are licensed under Apache 2.0 License.
+You can read more about the license in the LICENSE file in this
+folder.
--- src/read_lrtemplate.py
+++ src/read_lrtemplate.py
@@ -1,27 +1,48 @@
+'''Read LightRoom templates.
 
+This module contains functions that are essential to read
+LightRoom template files (.lrtemplate). These files
+are read first as plain text files, then, their contents
+are translated into JSON files. And this JSON version
+of the file is used.
+'''
 import re
 import json
+from typing import (
+    Dict, Any
+)
 
-TEMPLATE_KEY_REGEX = r'(?P<spaces>\s*)(?P<key>[A-Za-z0-9]+) = '
+KEY_IN_KV_PAIR_REGEX = r'(?P<spaces>\s*)(?P<key>[A-Za-z0-9]+) = '
 def read_text_file(path: str) -> str:
     with open(path, 'r', encoding="utf-8") as file_connection:
         content = file_connection.read()
     return content
 
 
-def read_lrtemplate(path: str) -> str:
+def read_lrtemplate(path: str) -> Dict[Any, Any]:
     template = read_text_file(path)
     template_lines = template.split('\n')
     for i, line in enumerate(template_lines):
-        match = re.search(TEMPLATE_KEY_REGEX, line)
+        match = re.search(KEY_IN_KV_PAIR_REGEX, line)
         if match:
             key = match['key']
             indent_level = match['spaces']
             as_json_key = str(indent_level) + f'"{key}": '
-            template_lines[i] = re.sub(TEMPLATE_KEY_REGEX, as_json_key, line, count = 1)
+            template_lines[i] = re.sub(KEY_IN_KV_PAIR_REGEX, as_json_key, line, count = 1)
 
-    l = '\n'.join(template_lines)
-    l = '{' + l + '}'
-    return json.loads(l)
+    template = '\n'.join(template_lines)
+    template = fix_objects_into_arrays(template)
+    template = '{' + template + '}'
+    template_as_json = json.loads(template)
+    if 's' in template_as_json.keys():
+        return template_as_json['s']
+    return template_as_json
 
-print(read_lrtemplate("assets/example_template.lrtemplate"))
+
+def fix_objects_into_arrays(template):
+    '''Util function to fix JSON objects that should be JSON arrays.'''
+    template = re.sub(r"([0-9]+),(\s*)\}(?!$)", r"\1]", template)
+    template = re.sub(r",(\s*)\}", r"}", template)
+    template = re.sub(r"\{(\s*)([0-9]+),", r"[\2,", template)
+    return template
+
