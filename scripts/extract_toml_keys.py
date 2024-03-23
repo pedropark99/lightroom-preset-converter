b40971797dd0c6825b59fbe5535fb5bf36e0f102
pedropark99 <pedropark99@gmail.com>
2024-03-23 15:26:09 -0300

    Update PP3 toml mapping

--- scripts/extract_toml_keys.py
+++ scripts/extract_toml_keys.py
@@ -1,30 +1,23 @@
 import re
 
 TOML_KEY_REGEX = r'^(?P<key>[A-Za-z0-9]+)( )?='
+TOML_SECTION_REGEX = r'^\[(?P<key>[A-Za-z0-9& ]+)\]'
+PP3_PATH = 'assets/rawtherapee-presets/IMG_7922.jpg.out.pp3'
 
-toml_text = '''
-Auto=false
-Clip=0.02
-Compensation=0
-Brightness=0
-Contrast=0
-Saturation=0
-Black=0
-HighlightCompr=0
-HighlightComprThreshold=0
-ShadowCompr=50
-HistogramMatching=true
-CurveFromHistogramMatching=true
-ClampOOG=true
-CurveMode=FilmLike
-CurveMode2=Standard
-Curve=4;0;0;0.050000000000000003;0.025915307334948689;0.12;0.094013162586586096;0.21799999999999997;0.26066079610420401;0.35519999999999996;0.54004663005862086;0.54727999999999999;0.8161151428792226;0.81619199999999992;0.97119514854912681;1;1;
-Curve2=0;
-'''
+with open(PP3_PATH, 'r', encoding='utf-8') as f:
+    toml_text = f.read()
 
-
+pp3_attrs = dict()
+current_section = None
 lines = toml_text.split('\n')
 for line in lines:
-    match = re.search(TOML_KEY_REGEX, line)
-    if match:
-        print(f"'{match['key']}',")
+    if re.search(TOML_SECTION_REGEX, line):
+        match = re.search(TOML_SECTION_REGEX, line)
+        current_section = match['key']
+        pp3_attrs[current_section] = list()
+    if re.search(TOML_KEY_REGEX, line):
+        match = re.search(TOML_KEY_REGEX, line)
+        pp3_attrs[current_section].append(match['key'])
+
+
+print(pp3_attrs)
